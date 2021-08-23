904beed9c2512b4ea50bba2a5a15f9ac3773117a
dvir200 <76834454+dvir200@users.noreply.github.com>
2021-08-23 11:47:35 +0300

    Create app.js

---
+++ JavaScript Sections/Section 16/Conditionals/app.js
@@ -0,0 +1,16 @@
+let random = Math.random();
+
+if (random < 0.5){
+  console.log("Your number is below 0.5");
+  console.log(random)
+}
+
+else{
+  console.log("Your number is above 0.5");
+  console.log(random);
+}
+
+/* if (random >= 0.5){
+  console.log("Your number is above 0.5");
+  console.log(random);
+} */
