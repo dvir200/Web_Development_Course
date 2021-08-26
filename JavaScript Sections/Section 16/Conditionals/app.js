1c17de0a5a3d82eacd922751ad9dd402285d4a3b
dvir200 <76834454+dvir200@users.noreply.github.com>
2021-08-26 11:49:43 +0300

    Update app.js

--- JavaScript Sections/Section 16/Conditionals/app.js
+++ JavaScript Sections/Section 16/Conditionals/app.js
@@ -1,4 +1,4 @@
-let random = Math.random();
+/* let random = Math.random();
 
 if (random < 0.5){
   console.log("Your number is below 0.5");
@@ -8,9 +8,21 @@ if (random < 0.5){
 else{
   console.log("Your number is above 0.5");
   console.log(random);
-}
+} */
 
 /* if (random >= 0.5){
   console.log("Your number is above 0.5");
   console.log(random);
 } */
+
+const age = 3;
+
+if (age<5){
+  console.log("You get in for free");
+} 
+else if(age <40){
+  console.log("You are an adult. you pay $20");
+}
+else if (age <90){
+  console.log("You are a senior. you pay $10");
+}
