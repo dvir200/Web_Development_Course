cfef9311d8087cbf887f99e1ee36e540461c7ed6
dvir200 <76834454+dvir200@users.noreply.github.com>
2021-09-06 11:48:09 +0300

    Create app.js

---
+++ JavaScript Sections/Section 19/Guessing Game/app.js
@@ -0,0 +1,38 @@
+let maximum = parseInt(prompt("Enter your maximum number: "));
+
+let tries = 1;
+
+while (!maximum){
+  maximum = parseInt(prompt("Invalid Number. Enter a new one: "));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) +1;
+/* console.log(targetNum); */
+let userInput = prompt("Enter a number to guess:");
+while (parseInt(userInput) !== targetNum){
+  if (userInput === 'q'){
+    break;
+  }
+  if (!userInput){
+    userInput = prompt("Invalid number. Enter a new one:");
+    tries ++;
+  }
+  else{
+    if (userInput > targetNum){
+      userInput = prompt("Too high. Enter a new number:");
+      tries ++;
+    }
+    else if (userInput < targetNum){
+      userInput = prompt("Too low. Enter a new number:");
+      tries ++;
+    }
+  }
+}
+
+if (userInput === 'q'){
+  console.log("You quit the game");
+}
+else{
+  console.log(`You guessed the number. It was ${targetNum}.`);
+  console.log(`It took you ${tries} tries.`);
+}
