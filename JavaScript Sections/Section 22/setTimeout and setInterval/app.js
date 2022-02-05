d0f0ad455fc608167e608a9acdee4414c6a9c249
dvir200 <76834454+dvir200@users.noreply.github.com>
2022-02-05 11:29:48 +0200

    Create app.js

---
+++ JavaScript Sections/Section 22/setTimeout and setInterval/app.js
@@ -0,0 +1,13 @@
+console.log("Hi");
+
+/* setTimeout(() =>{
+  console.log("Are you still there?")
+}, 3000) */
+
+const id = setInterval(() =>{
+  console.log("Are you still there?")
+}, 3000)
+
+const id2 = setInterval(() =>{
+  console.log("Goodbye")
+}, 3000)
